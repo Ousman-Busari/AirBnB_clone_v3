6c3079b4473d9d865ccf09dec2c07b1a05b835d9
Ousman <usman.traid@gmail.com>
2023-02-16 07:42:17 -0500

    amenities view and CRUD implemented

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+amenities object views and CRUD
+"""
 from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
