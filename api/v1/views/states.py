261c5d134d56e5118fb5f22793f444006860551c
Ousman <usman.traid@gmail.com>
2023-02-16 06:51:11 -0500

    cities views and CRUD implemented

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,7 +6,6 @@ from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
 from models.state import State
-import json
 
 
 @app_views.route("/states", strict_slashes=False)
