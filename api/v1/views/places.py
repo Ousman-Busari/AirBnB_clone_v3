fb31d8e81878d552d5596ca282237df514fb007e
Ousman <usman.traid@gmail.com>
2023-02-16 09:34:43 -0500

    Place objects views and CRUD implemented

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,58 +5,66 @@ City objects view implementation
 from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.state import City
+from models.place import Place
+from models.city import City
+from models.user import User
 
 
-@app_views.route("/states/<state_id>/cities", strict_slashes=False)
-def get_cities(state_id):
-    """ Retrieves the list of all cities of a State """
-    state = storage.get(State, state_id)
+@app_views.route("/cities/<city_id>/places", strict_slashes=False)
+def get_places(city_id):
+    """ Retrieves the list of all Place object uner a City Object """
+    city = storage.get(City, city_id)
 
-    if state is None:
+    if city is None:
         abort(404)
 
-    state_cities = [city.to_dict() for city in state.cities]
-    return jsonify(state_cities)
+    city_places = [place.to_dict() for place in city.places]
+    return jsonify(city_places)
 
 
-@app_views.route("/states/<state_id>/cities", methods=["POST"],
+@app_views.route("/cities/<city_id>/places", methods=["POST"],
                  strict_slashes=False)
-def post_city_under_state(state_id):
-    state = storage.get(State, state_id)
+def post_place_under_city(city_id):
+    """ Posts/creates a new Place object under a City Object """
+    city = storage.get(City, city_id)
 
-    if state is None:
+    if city is None:
         abort(404)
 
     req_body = request.get_json()
     if type(req_body) is not dict:
         abort(400, "Not a JSOn")
 
+    if "user_id" not in req_body:
+        abort(400, "Missing user_id")
+
+    if storage.get(User, req_body.user_id) is None:
+        abort(404)
+
     if "name" not in req_body:
         abort(400, "Missing name")
 
-    req_body.update({"state_id": state_id})
-    new_city = City(**req_body)
-    storage.new(new_city)
+    req_body.update({"city_id": city_id})
+    new_place = Place(**req_body)
+    storage.new(new_place)
     storage.save()
-    return jsonify(new_city.to_dict()), 201
+    return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route("/cities/<city_id>", methods=["GET", "DELETE", "PUT"],
+@app_views.route("/places/<place_id>", methods=["GET", "DELETE", "PUT"],
                  strict_slashes=False)
-def rud_city(city_id):
+def rud_place(place_id):
     """ reads, updates, and deletes a City object """
-    city = storage.get(City, city_id)
+    place = storage.get(Place, place_id)
 
-    if city is None:
+    if place is None:
         abort(404)
 
     if request.method == "GET":
-        return jsonify(city.to_dict())
+        return jsonify(place.to_dict())
 
     if request.method == "DELETE":
-        storage.delete(city)
+        storage.delete(place)
         storage.save()
         return jsonify({}), 200
 
@@ -67,9 +75,9 @@ def rud_city(city_id):
             abort(400, "Not a JSON")
 
         for key, value in req_body.items():
-            if (key != "id" and key != "created_at" and
-               key != "updated_at"):
-                setattr(city, key, value)
+            if (key != "id" and key != "user_id" and key != "city_id" and
+               key != "created_at" and key != "updated_at"):
+                setattr(place, key, value)
 
         storage.save()
-        return jsonify(city.to_dict()), 200
+        return jsonify(place.to_dict()), 200
