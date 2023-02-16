00cc54233b19cc620969accf1e7a5bc93f24b465
Ousman <usman.traid@gmail.com>
2023-02-16 10:34:21 -0500

    cross-origin resource sharing(CORS) configured

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,7 @@ from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
