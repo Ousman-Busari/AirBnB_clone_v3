82e9b03fce86bc228c9e762011f06444cc5a73f0
Ousman <usman.traid@gmail.com>
2023-02-16 05:02:02 -0500

    State objects CRUD implmented

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 API setup with flask
 """
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,10 +11,17 @@ from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close_app(exception):
     """ closes the app and all connections """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(err):
+    """ returns a json as a 404 response """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
---
+++ api/v1/views/states.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+from flask import jsonify, abort, request, make_response
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+import json
+
+
+@app_views.route("/states", strict_slashes=False)
+def get_states():
+    """ Retrives all state object from the storage"""
+    all_states_in_list = list(storage.all(State).values())
+    list_all_states_obj = [
+        state.to_dict() for state in all_states_in_list
+    ]
+    return jsonify(list_all_states_obj)
+
+
+@app_views.route("/states", methods=["POST"], strict_slashes=False)
+def post_state():
+    """ Creates and add a new state object to the storage """
+    req_body = request.get_json()
+    if type(req_body) is not dict:
+        abort(400, "Not a JSON")
+
+    if "name" not in req_body:
+        abort(400, "Missing name")
+
+    new_state = State(**req_body)
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route("/states/<state_id>", methods=["GET", "DELETE", "PUT"],
+                 strict_slashes=False)
+def state_action(state_id):
+    """ retreives and manipulate a State object """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+
+    if request.method == "GET":
+        return jsonify(state.to_dict())
+
+    if request.method == "DELETE":
+        storage.delete(state)
+        return jsonify({}), 200
+
+    if request.method == "PUT":
+        req_body = request.get_json()
+        if type(req_body) is not dict:
+            abort(400, "Not a JSON")
+
+        for key, value in req_body.items():
+            if (key is not "id" and key is not "created_at" and
+               key is not "updated_at"):
+                setattr(state, key, value)
+        storage.save()
+        return jsonify(state.to_dict())
