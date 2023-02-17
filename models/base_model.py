97c993d596e35bcc9a4440f27a54d723bd2edd42
Ousman <usman.traid@gmail.com>
2023-02-16 23:58:16 -0500

    user password encrypted

--- models/base_model.py
+++ models/base_model.py
@@ -70,7 +70,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if self.__class__.__name__ == "User" and getenv("HBNB_TYPE_STORAGE") == "db":
+        if self.__class__.__name__ == "User":
             del new_dict["password"]
         return new_dict
 
